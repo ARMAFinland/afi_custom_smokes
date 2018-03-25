c98d601a3a3ef35e6a98d51f8a5ed4c51c5435ae
Johnson <jan_319@msn.com>
2018-03-26 00:39:38 +0300

    update
    GL smoke bouncing fix

--- addons/afi_custom_smokes/config.cpp
+++ addons/afi_custom_smokes/config.cpp
@@ -3,41 +3,16 @@ class CfgPatches {
 		units[] = {};
 		weapons[] = {};
 		requiredVersion = 0.1;
-		requiredAddons[] = {"A3_Weapons_F","rhsusf_c_weapons","rhs_c_weapons","rhs_c_heavyweapons"};
-	};
-};
-
-class CfgFunctions {
-	class AFI {
-		tag = "AFI";
-		class WeaponsEH {
-			class effectFiredSmokeGenerator{
-				file = "\afi_custom_smokes\effects\afi_effectFiredSmokeGenerator.sqf";
-				description = "Effects for smoke generator";
-			};
-		};
-	};
-};
-
-class CfgRemoteExec {
-	class Functions {
-		mode = 2;
-		jip = 1;
-		class afi_fnc_effectFiredSmokeLauncher {
-			allowedTargets = 0;
-		};
-		class afi_fnc_effectFiredSmokeGenerator {
-			allowedTargets = 0;
-		};
-		class afi_effectFiredSmokeLauncherUS {
-			allowedTargets = 0;
-		};
-	};
+		requiredAddons[] = {"A3_Weapons_F","rhsusf_c_weapons","rhs_c_weapons","ace_particles"};
+ 	};
 };
 
 class CfgAmmo {
 	class GrenadeHand;
-	class SmokeLauncherAmmo;
+	class Grenade;
+	class SmokeShellRed;
+	class SmokeShellGreen;
+	class SmokeShellYellow;
 	class SmokeShell: GrenadeHand {
 		deflecting =70;
 		deflectionSlowDown=0.3;
@@ -45,16 +20,49 @@ class CfgAmmo {
 		simulation = "shotSmoke";
 	};
 
-	//Vehicle smokes
-	class rhs_ammo_smokegen: SmokeLauncherAmmo {
-		muzzleEffect = "";
-		rhs_muzzleEffect = "afi_fnc_effectFiredSmokeGenerator";
-		weaponLockSystem = "2";
-		simulation = "shotCM";
-		AIAmmoUsageFlags = "4";
-		hit = 0;
-		indirectHit = 0;
-		indirectHitRange = 0;
+	//USF
+	//GL smokes
+	class rhs_40mm_smoke_red: SmokeShellRed {
+		smokeColor[] = {0.9,0.1,0.1,0.3};
+	};
+	class rhs_40mm_smoke_green: SmokeShellGreen	{
+		smokeColor[] = {0.2,0.9,0.2,0.3};
+	};
+	//Throwable
+	class rhs_ammo_m18_green: SmokeShell {
+		smokeColor[] = {0.2,0.9,0.2,0.3};
+	};
+	class rhs_ammo_m18_purple: rhs_ammo_m18_green {
+		smokeColor[] = {0.4341,0.1388,0.4144,0.3};
+	};
+	class rhs_ammo_m18_red: rhs_ammo_m18_green {
+		smokeColor[] = {0.9,0.1,0.1,0.3};
+	};
+	class rhs_ammo_m18_yellow: rhs_ammo_m18_green {
+		smokeColor[] = {0.9883,0.8606,0.0719,0.3};
+	};
+
+	//AFRF
+	//GL smokes
+	class rhs_g_vog25;
+	class rhs_g_vg40md_white: rhs_g_vog25 {
+		deflecting =70;
+		deflectionSlowDown=0.3;
+		simulation = "shotSmoke";
+	};
+	class rhs_g_vg40md_green: rhs_g_vg40md_white {
+		smokeColor[] = {0.2,0.9,0.2,0.3};
+	};
+	class rhs_g_vg40md_red: rhs_g_vg40md_white {
+		smokeColor[] = {0.9,0.1,0.1,0.3};
+	};
+};
+
+class CfgCloudlets {
+	class Default;
+	class ACE_SmokeBaseLarge: Default {
+		lifeTime = 45;
+		lifeTimeVar = 15;
 	};
 };
 
